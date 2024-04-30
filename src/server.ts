bafff55fcda12d3e3bde38b35acf5b15d048d251
reazul7 <islamreazul7777@gmail.com>
2024-04-30 23:29:04 +0600

    setup lint-staged update

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './config';
 
 async function main() {
     try {
-        await mongoose.connect(config.database_url as string);
+        await mongoose.connect(config.database_url as string)
         console.log('Database connected successfully');
 
         app.listen(config.port, () => {
